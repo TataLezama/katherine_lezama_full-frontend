7993a87765ebcd9f13e3174c3d74485a2e757623
Katherine <lezamakatherine@gmail.com>
2025-09-23 17:49:21 -0500

    Cambiar en response_type 'token' por 'code'

--- src/spotify/auth.tsx
+++ src/spotify/auth.tsx
@@ -6,7 +6,7 @@ const scopes = [
   "user-read-email",
 ];
 
-export const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(
+export const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(
   redirectUri
 )}&scope=${scopes.join("%20")}`;
 

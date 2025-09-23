b03d0c7b2c92f924a548aa1809007ec10174f999
Katherine <lezamakatherine@gmail.com>
2025-09-23 16:00:05 -0500

    Ajuste url de petición para token de autorización

--- src/spotify/auth.tsx
+++ src/spotify/auth.tsx
@@ -3,14 +3,13 @@ const clientId = "3101fd6a7ac84f3795ba053c6cbe44b5"; // Reemplázalo con tu Clie
 const redirectUri = "https://katherine-lezama-full-frontend.vercel.app/callback"; // Debe coincidir con el registrado en Spotify
 const scopes = [
   "user-read-email",
-  "user-read-private",
-  "user-top-read",
+  "user-library-read",
   "playlist-read-private",
 ];
 
 export const loginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&redirect_uri=${encodeURIComponent(
   redirectUri
-)}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`;
+)}&scope=${scopes.join("%20")}`;
 
 export const getTokenFromUrl = (): string | null => {
   const hash = window.location.hash.substring(1); // obtiene lo que está después de "#"
